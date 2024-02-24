cee867ea50cd806bbd29978667cfd6e50f107854
gabrielabade <bade.gabriela@gmail.com>
2024-02-24 14:05:44 -0300

    baseURL alterada

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: ' https://codeburger-backend-production-ddd5.up.railway.app/'
 })
 
 api.interceptors.request.use(async config => {
